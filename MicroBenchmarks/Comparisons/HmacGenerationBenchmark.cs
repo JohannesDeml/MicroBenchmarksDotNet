68236a248935bf68e043b66b8914ce57e35bb5ef
Johannes Deml <public@deml.io>
2021-03-02 12:34:00 +0100

    Add hmac benchmark

---
+++ MicroBenchmarks/Comparisons/HmacGenerationBenchmark.cs
@@ -0,0 +1,105 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HmacGenerationBenchmark.cs">
+//   Copyright (c) 2021 Johannes Deml. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Extensions;
+using MicroBenchmarks.Extensions;
+
+namespace MicroBenchmarks
+{
+	[Config(typeof(DefaultBenchmarkConfig))]
+	public class HmacGenerationBenchmark
+	{
+		[Params(10, 100, 1000)]
+		public int ArraySize { get; set; }
+
+		private byte[] secretKey;
+		private byte[] data;
+		private byte[] hashResult;
+
+		private HMACMD5 md5Provider;
+		private HMACSHA1 sha1Provider;
+		private HMACSHA256 sha256Provider;
+
+		[GlobalSetup]
+		public void PrepareBenchmark()
+		{
+			secretKey = new byte[64];
+			using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+			{
+				rng.GetBytes(secretKey);
+			}
+
+			data = ValuesGenerator.Array<byte>(ArraySize);
+
+			md5Provider = new HMACMD5(secretKey);
+			sha1Provider = new HMACSHA1(secretKey);
+			sha256Provider = new HMACSHA256(secretKey);
+			hashResult = new byte[64];
+		}
+
+		[Benchmark]
+		public byte[] Md5Hmac()
+		{
+			var result = md5Provider.ComputeHash(data, 0, data.Length);
+			return result;
+		}
+
+		[Benchmark]
+		public byte[] Sha1Hmac()
+		{
+			var result = sha1Provider.ComputeHash(data, 0, data.Length);
+			return result;
+		}
+
+		[Benchmark]
+		public byte[] Sha256Hmac()
+		{
+			var result = sha256Provider.ComputeHash(data, 0, data.Length);
+			return result;
+		}
+
+
+		[Benchmark]
+		public byte[] TryMd5Hmac()
+		{
+			if (md5Provider.TryComputeHash(data, hashResult, out int bytesWritten))
+			{
+				return hashResult;
+			}
+
+			throw new InvalidOperationException("Error when computing HMAC");
+		}
+
+		[Benchmark]
+		public byte[] TrySha1Hmac()
+		{
+			if (sha1Provider.TryComputeHash(data, hashResult, out int bytesWritten))
+			{
+				return hashResult;
+			}
+
+			throw new InvalidOperationException("Error when computing HMAC");
+		}
+
+		[Benchmark]
+		public byte[] TrySha256Hmac()
+		{
+			if (sha256Provider.TryComputeHash(data, hashResult, out int bytesWritten))
+			{
+				return hashResult;
+			}
+
+			throw new InvalidOperationException("Error when computing HMAC");
+		}
+	}
+}
