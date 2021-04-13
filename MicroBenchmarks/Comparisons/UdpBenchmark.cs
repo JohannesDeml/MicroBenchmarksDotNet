5ce92bb08e9b16060ee30e018fffb7737e286f92
Johannes Deml <public@deml.io>
2021-04-13 10:06:02 +0200

    Add simple udp socket benchmark
    Has some very interesting results for safe mode, is a lot faster there than in normal windows mode

---
+++ MicroBenchmarks/Comparisons/UdpBenchmark.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UdpBenchmark.cs">
+//   Copyright (c) 2021 Johannes Deml. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks.Extensions;
+
+namespace MicroBenchmarks
+{
+	[Config(typeof(DefaultBenchmarkConfig))]
+	public class UdpBenchmark
+	{
+		[Params(32, 1_000, 10_000)]
+		public int MessageSize { get; set; }
+
+		private int port = 3333;
+
+		UdpClient udpServer;
+		private IPEndPoint endPoint;
+		UdpClient udpClient;
+		private byte[] message;
+
+		[GlobalSetup]
+		public void PrepareBenchmark()
+		{
+			PrepareServer();
+			PrepareClient();
+		}
+
+		private void PrepareServer()
+		{
+			udpServer = new UdpClient(port);
+			endPoint = new IPEndPoint(IPAddress.Any, port);
+		}
+
+		private void PrepareClient()
+		{
+			udpClient = new UdpClient();
+			IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+			udpClient.Connect(ep);
+
+			message = new byte[MessageSize];
+			var random = new Random(0);
+			random.NextBytes(message);
+		}
+
+		[Benchmark]
+		public int SendReceive()
+		{
+			udpClient.Send(message, message.Length);
+			var receivedData = udpServer.Receive(ref endPoint);
+
+			return receivedData.Length;
+		}
+	}
+}
