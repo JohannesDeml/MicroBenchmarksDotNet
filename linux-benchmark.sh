0059026cd54d509e7c3774df4544511c5881a31b
Johannes Deml <public@deml.io>
2021-05-06 15:06:56 +0200

    Revert "Change linux shell to use .NET 5 as target"
    This reverts commit 3d633476dff487f988360615233892155864655a.

--- linux-benchmark.sh
+++ linux-benchmark.sh
@@ -7,8 +7,8 @@
 export MONO_UNITY="/home/johannes/Unity/Hub/Editor/2020.2.6f1/Editor/Data/MonoBleedingEdge/bin-linux64/mono"
 
 # Needs to be compiled as an exe, since benchmarkdotnet .netcore builds can't run mono targets 
-dotnet build --configuration Release --framework net5.0 --output ./bin/MicroBenchmarks-Linux/
-./bin/MicroBenchmarks-Linux/MicroBenchmarks
+dotnet build --configuration Release --framework net48 --output ./bin/MicroBenchmarks-Linux/
+mono ./bin/MicroBenchmarks-Linux/MicroBenchmarks.exe
 
 echo "--- Benchmark finished ---"
 echo "Save current process list"
