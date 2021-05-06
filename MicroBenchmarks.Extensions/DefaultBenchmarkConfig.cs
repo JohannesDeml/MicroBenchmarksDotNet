8ca688ebd843d37d441f483354a60b1b4677dc70
Johannes Deml <public@deml.io>
2021-05-06 15:05:42 +0200

    Use all targets in main branch

--- MicroBenchmarks.Extensions/DefaultBenchmarkConfig.cs
+++ MicroBenchmarks.Extensions/DefaultBenchmarkConfig.cs
@@ -27,7 +27,10 @@ namespace MicroBenchmarks.Extensions
 			var baseJob = DefineBaseJob();
 
 			AddJob(baseJob.WithRuntime(CoreRuntime.Core50));
-
+			AddJob(baseJob.WithRuntime(CoreRuntime.Core31));
+			AddJob(baseJob.WithRuntime(ClrRuntime.Net48));
+			AddMonoJob(baseJob);
+			
 			ConfigHelper.AddDefaultColumns(this);
 		}
 
