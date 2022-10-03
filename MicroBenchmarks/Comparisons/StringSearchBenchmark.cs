9d742e7f8a0deb97d569f0026b8ba23f69ec6174
Johannes Deml <public@deml.io>
2022-10-03 22:21:30 +0200

    Add a benchmark to compare string search vs. char search

---
+++ MicroBenchmarks/Comparisons/StringSearchBenchmark.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StringSearchBenchmark.cs">
+//   Copyright (c) 2022 Johannes Deml. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks.Extensions;
+
+namespace MicroBenchmarks
+{
+	[Config(typeof(DefaultBenchmarkConfig))]
+	public class StringSearchBenchmark
+	{
+		[Params(11, 10001)]
+		public int Length { get; set; }
+
+		public const char TargetChar = '|';
+		public const string TargetString = "|";
+		private string stringData;
+
+		[GlobalSetup]
+		public void PrepareBenchmark()
+		{
+			Random random = new Random();
+			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+			stringData = new string(Enumerable.Repeat(chars, Length -1)
+				.Select(s => s[random.Next(s.Length)]).ToArray());
+			stringData = stringData.Insert((int)Math.Floor(Length / 2.0f), TargetString);
+		}
+
+
+		[Benchmark]
+		public int IndexOfString()
+		{
+			return stringData.IndexOf(TargetString, StringComparison.Ordinal);
+		}
+
+		[Benchmark]
+		public int LastIndexOfString()
+		{
+			return stringData.LastIndexOf(TargetString, StringComparison.Ordinal);
+		}
+
+		[Benchmark]
+		public int IndexOfChar()
+		{
+			return stringData.IndexOf(TargetChar);
+		}
+
+		[Benchmark]
+		public int LastIndexOfChar()
+		{
+			return stringData.LastIndexOf(TargetChar);
+		}
+	}
+}
