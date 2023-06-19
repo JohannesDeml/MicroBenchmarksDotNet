ad8b27a4a8c75832269fd39917a7ae6d6d53114f
Johannes Deml <public@deml.io>
2023-06-19 20:06:06 +0200

    Add string concatenation benchmark

---
+++ MicroBenchmarks/Comparisons/Strings/StringConcatenationBenchmark.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Extensions;
+using MicroBenchmarks.Extensions;
+
+namespace MicroBenchmarks
+{
+	/// <summary>
+	/// Compare classic string concatenation with string builder (with new instances created in each method)
+	/// </summary>
+	[Config(typeof(DefaultBenchmarkConfig))]
+	public class StringConcatenationBenchmark
+	{
+		[Params(5, 100)]
+		public int StringCount { get; set; }
+
+		[Params(10, 1_000)]
+		public int StringLength { get; set; }
+
+		private string[] stringArray;
+
+		[GlobalSetup]
+		public void PrepareBenchmark()
+		{
+			stringArray = new string[StringCount];
+			for (int i = 0; i < stringArray.Length; i++)
+			{
+				stringArray[i] = ValuesGenerator.GenerateRandomString(StringLength);
+			}
+		}
+
+		[Benchmark]
+		public string StringConcatenation()
+		{
+			string completeString = String.Empty;
+			for (var i = 0; i < stringArray.Length; i++)
+			{
+				var part = stringArray[i];
+				completeString += part;
+			}
+
+			return completeString;
+		}
+
+		[Benchmark]
+		public string StringBuilderAppend()
+		{
+			StringBuilder sb = new StringBuilder();
+			for (var i = 0; i < stringArray.Length; i++)
+			{
+				var part = stringArray[i];
+				sb.Append(part);
+			}
+
+			return sb.ToString();
+		}
+
+		[Benchmark]
+		public string StringBuilderInsert()
+		{
+			StringBuilder sb = new StringBuilder();
+			for (var i = stringArray.Length - 1; i >= 0; i--)
+			{
+				var part = stringArray[i];
+				sb.Insert(0, part);
+			}
+
+			return sb.ToString();
+		}
+	}
+}
