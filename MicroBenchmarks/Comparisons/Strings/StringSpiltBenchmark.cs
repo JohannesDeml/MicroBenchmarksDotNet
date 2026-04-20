a356d7c1585b988db03fb736378800506293e3b1
Johannes Deml <johannes.deml@innogames.com>
2026-04-20 17:17:36 +0200

    Add simple test to check how the performance of split is when used with a char or with a string

---
+++ MicroBenchmarks/Comparisons/Strings/StringSpiltBenchmark.cs
@@ -0,0 +1,45 @@
+using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Extensions;
+using MicroBenchmarks.Extensions;
+
+namespace MicroBenchmarks
+{
+	[Config(typeof(DefaultBenchmarkConfig))]
+	public class StringSpiltBenchmark
+	{
+		[Params(100, 10_000)]
+		public int StringLength { get; set; }
+
+		[Params(1, 100)]
+		public int NumSeparators { get; set; }
+
+
+		private char TargetChar = '|';
+		private string TargetString = "|";
+		private string stringData;
+
+		[GlobalSetup]
+		public void PrepareBenchmark()
+		{
+			stringData = ValuesGenerator.GenerateRandomString(StringLength - NumSeparators);
+			// Spread out targets evenly across the string
+			for (int i = 0; i < NumSeparators; i++)
+			{
+				stringData = stringData.Insert((StringLength / NumSeparators) * i, TargetString);
+			}
+		}
+
+		[Benchmark]
+		public string[] SplitByChar()
+		{
+			return stringData.Split(TargetChar);
+		}
+
+		[Benchmark]
+		public string[] SplitByString()
+		{
+			return stringData.Split(TargetString);
+		}
+	}
+}
